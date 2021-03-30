aa35eaf42d034a173d02fa0b0b7de7fe1fe41ddb
toto <tgcyberdragon@gmail.com>
2021-03-30 17:27:13 +0200

    suite du http basic avec bcrypt mais ne fonctionne pas

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -116,7 +116,7 @@ class ProStageController extends AbstractController
         }
 
         /**
-        * @Route("/admin/stage/ajout", name="pro_stage_stages_ajout")
+        * @Route("/admin/profile/stage/ajout", name="pro_stage_stages_ajout")
         */
         public function ajouterStage(Request $request, EntityManagerInterface $manager): Response
         {
