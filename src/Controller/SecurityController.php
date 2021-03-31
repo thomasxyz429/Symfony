fbbfb617c469d3b5e906f9e45f31a8d3e6f1124f
toto <tgcyberdragon@gmail.com>
2021-03-31 17:48:11 +0200

    ajout inscription

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,7 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
+use App\Form\UserType;
 class SecurityController extends AbstractController
 {
     /**
@@ -30,7 +33,30 @@ class SecurityController extends AbstractController
      * @Route("/logout", name="app_logout")
      */
     public function logout()
+    {}
+
+    /**
+    * @Route("/Inscription", name="app_inscription")
+    */
+    public function inscription(Request $request, EntityManagerInterface $manager): Response
     {
 
+      // Créer un utilisateur vide
+      $user = new User();
+
+      //Creation d'un formulaire
+      $formulaireUser= $this->createForm(UserType::class, $user);
+
+      $formulaireUser->handleRequest($request);
+      if ($formulaireUser->isSubmitted()&& $formulaireUser->isValid()) {
+      //Enregistrer en Base de Données
+      $manager->persist($user);
+      $manager->flush();
+      //Rediriger sur la page d'acceuil
+      return $this->redirectToRoute('pro_stage');
+      }
+      //afficher la page presentant le formulaire d'inscription
+
+      return $this->render('security/inscription.html.twig', ['vueFormulaireUser' => $formulaireUser->createView()]);
     }
 }
