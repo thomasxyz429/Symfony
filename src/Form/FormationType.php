82ac94260d00ac0d6c31ed4309a3aa7d7359e5d6
toto <tgcyberdragon@gmail.com>
2021-03-31 18:34:08 +0200

    test

---
+++ src/Form/FormationType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Formation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('idFormation')
+            ->add('intitule')
+            ->add('niveau')
+            ->add('ville')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Formation::class,
+        ]);
+    }
+}
